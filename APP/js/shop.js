acfa316f0b4572f741e1c1edad9c4ac99e77c4cb
Xymfromthecatstar <1806086063@qq.com>
2017-11-15 15:41:39 +0800

    2

---
+++ APP/js/shop.js
@@ -0,0 +1,31 @@
+
+var arrImg = ["img/hw.png","img/nb.png","img/xz.png"];
+var num = 0;
+
+$(".lists").children().click(function(){
+//	$(this).addClass("active").siblings().removeClass("active");
+	var q =$(this).index();
+	$('.buttonSp>div').eq(q).css({'display':'block'}).siblings().css({'display':'none'});
+})
+
+function autoImg(){
+				num++;
+				if (num<3) {
+					$('.Carousel img').eq(0).prop("src",arrImg[num]);
+				} else{
+					num=0;
+					$('.Carousel img').eq(0).prop("src",arrImg[num]);
+				}
+			}
+			
+		var timer = setInterval(autoImg,3000);
+
+
+
+
+
+
+
+
+
+
